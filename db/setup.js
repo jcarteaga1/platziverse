b29fc89c6fc922e041d5f48bb4952c23bcc70446
Juanc Arteaga <arteagajr07@gmail.com>
2021-07-05 15:42:52 -0500

    use chalk to using color in error and inquiererto question comfirm

--- db/setup.js
+++ db/setup.js
@@ -1,11 +1,27 @@
 'use strict'
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./index')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'this will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
-    username: process.env.DB_USER || 'platzi',
+    username: process.env.DB_USER || 'platzis',
     password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
@@ -20,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
