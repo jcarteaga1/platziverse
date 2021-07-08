57bb789d29ca286e40f8cf2c8fe95fe0c8e6d2b4
Juanc Arteaga <arteagajr07@gmail.com>
2021-07-07 22:32:42 -0500

    fix in setup db

--- db/setup.js
+++ db/setup.js
@@ -21,7 +21,7 @@ async function setup () {
 
   const config = {
     database: process.env.DB_NAME || 'platziverse',
-    username: process.env.DB_USER || 'platzis',
+    username: process.env.DB_USER || 'platzi',
     password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
